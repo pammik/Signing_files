8234c4dd7f6a8a4f57f6e53799eebac2e9372832
Алексей Плотников <a.plotnikov.m@gmail.com>
2023-12-03 23:52:14 +0100

    fixxa

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
+import os
+import io
 import shutil
 import socket
 from typing import List
 from pathlib import Path
-from fastapi.responses import JSONResponse
+from fastapi.responses import JSONResponse, FileResponse
 from fastapi import FastAPI, UploadFile, File, HTTPException
 from copy_fs import copy_files
+import tempfile
 
 upload_folder = Path("Download")
 upload_folder.mkdir(parents=True, exist_ok=True)
@@ -18,6 +21,12 @@ buildagents = [
     {"id": 1, "role": "agent", "folder": "ba-01"},
     {"id": 2, "role": "agent", "folder": "ba-02"}
 ]
+
+def create_zip_archive(directory_path: str, zip_filename: str):
+    with tempfile.TemporaryDirectory() as temp_dir:
+        temp_zip_path = os.path.join(temp_dir, zip_filename)
+        shutil.make_archive(temp_zip_path, 'zip', directory_path)
+        return temp_zip_path + '.zip'
 
 
 @app.get("/")
@@ -58,6 +67,22 @@ async def read_file(filename: str):
         raise HTTPException(status_code=404, detail="File not found")
 
     return JSONResponse(content={"file_path": str(file_path)}, status_code=200)
+
+
+@app.get("/download/{dir_path:path}")
+async def download_directory(dir_path: str):
+    # Validate that the directory exists
+    aa = f"F:\git\Signing_files\Download\{dir_path}"
+    if not os.path.exists(aa) or not os.path.isdir(aa):
+        raise HTTPException(status_code=404, detail="Directory not found")
+
+    zip_filename = f"{os.path.basename(dir_path)}_archive"
+
+    # Create the zip archive
+    zip_path = create_zip_archive(dir_path, zip_filename)
+
+    # Return the zip archive as a FileResponse
+    return FileResponse(zip_path, filename=zip_filename + ".zip", media_type="application/zip")
 
 
 @app.get("/ready_signing_files")
